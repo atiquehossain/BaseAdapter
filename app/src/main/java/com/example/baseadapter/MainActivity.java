a45e9afa0e579a73c903ee3e8bb8caf387bf5103
Atique <atique.hossain@savethechildren.org>
2021-07-30 13:22:32 +0600

    Initial commit and done

---
+++ app/src/main/java/com/example/baseadapter/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.baseadapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String []bikename={"Kawasiki","BMW","TVS"};
+    int [] bikepicture={R.drawable.kaw,R.drawable.bmw,R.drawable.tvs};
+    GridView gridView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MyCustomBaseAdapter myCustomBaseAdapter
+                =new MyCustomBaseAdapter(this,bikename,bikepicture);
+
+        gridView=findViewById(R.id.gridview);
+        gridView.setAdapter(myCustomBaseAdapter);
+
+    }
+}
---
+++ app/src/main/java/com/example/baseadapter/MyCustomBaseAdapter.java
@@ -0,0 +1,56 @@
+package com.example.baseadapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MyCustomBaseAdapter extends BaseAdapter {
+    View view;
+    ImageView imageView;
+    TextView textView;
+    Context context;
+    String []bike;
+    int [] bikeimg;
+
+    public MyCustomBaseAdapter(Context context, String[] bike, int[] bikeimg) {
+        this.context = context;
+        this.bike = bike;
+        this.bikeimg = bikeimg;
+    }
+
+    @Override
+    public int getCount() {
+        return bike.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return bikeimg.length;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        LayoutInflater layoutInflater=(LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        view=layoutInflater.inflate(R.layout.layouttwo,null);
+
+        imageView=(ImageView)view.findViewById(R.id.imageview);
+        textView=(TextView) view.findViewById(R.id.textview);
+
+        imageView.setImageResource((bikeimg[i]));
+        textView.setText(bike[i]);
+
+
+        return view;
+
+
+    }
+}
